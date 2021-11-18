710ba64e7bc4b60595201cf30b7746575abdbcf8
Samuel Höra <samuelhoera@gmail.com>
2021-11-18 20:10:52 +0100

    chore: update all messages if flag is set

--- src/hllevents/discord/hllevent-discord.service.ts
+++ src/hllevents/discord/hllevent-discord.service.ts
@@ -30,7 +30,10 @@ export class HLLEventsDiscordService {
     this.logger.warn('Updating existing events using compatibility mode');
 
     const events = await this.eventRepository.getOpenEvents();
-    events.forEach((event) => this.updateInformationMessage(event));
+    events.forEach((event) => {
+      this.updateInformationMessage(event);
+      this.updateEnrolmentMessage(event);
+    });
   }
 
   async publishMessages(event: HLLEvent) {
