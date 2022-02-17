900972889a45e612f81a0f1b99b6d96a88860ac1
Samuel Höra <samuel@hoera.dev>
2022-02-17 18:58:57 +0100

    fix: typeorm imports

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Member } from '../typeorm/entities';
+import { Contact, Member } from '../typeorm/entities';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Member])],
+  imports: [TypeOrmModule.forFeature([Member, Contact])],
   controllers: [UsersController],
   providers: [UsersService],
   exports: [UsersService],
